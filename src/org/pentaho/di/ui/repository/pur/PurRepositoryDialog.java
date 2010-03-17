6167d9634df24db00a77c7390230782fd39bbf21
Ramaiz Mansoor <rmansoor@pentaho.com>
2010-03-17 21:33:50 +0000

    PDI-3383 - Change the default url path for the repository and other webservices in PDI PDI-3371 - Replaced the repository config dialog with a xul one and made ui cleanup PDI-3884 - URL for the webservice is now in the properties file

--- src/org/pentaho/di/ui/repository/pur/PurRepositoryDialog.java
+++ src/org/pentaho/di/ui/repository/pur/PurRepositoryDialog.java
@@ -1,261 +1,135 @@
 package org.pentaho.di.ui.repository.pur;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.ShellAdapter;
-import org.eclipse.swt.events.ShellEvent;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.DirectoryDialog;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.MessageBox;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 import org.pentaho.di.core.Const;
+import org.pentaho.di.core.gui.SpoonFactory;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.repository.RepositoriesMeta;
 import org.pentaho.di.repository.RepositoryMeta;
-import org.pentaho.di.repository.pur.PurRepositoryLocation;
 import org.pentaho.di.repository.pur.PurRepositoryMeta;
-import org.pentaho.di.ui.core.PropsUI;
-import org.pentaho.di.ui.core.gui.GUIResource;
-import org.pentaho.di.ui.core.gui.WindowProperty;
 import org.pentaho.di.ui.repository.dialog.RepositoryDialogInterface;
-import org.pentaho.di.ui.trans.step.BaseStepDialog;
+import org.pentaho.di.ui.repository.pur.controller.RepositoryConfigController;
+import org.pentaho.ui.xul.XulDomContainer;
+import org.pentaho.ui.xul.XulException;
+import org.pentaho.ui.xul.XulRunner;
+import org.pentaho.ui.xul.containers.XulDialog;
+import org.pentaho.ui.xul.swt.SwtXulLoader;
+import org.pentaho.ui.xul.swt.SwtXulRunner;
 
 public class PurRepositoryDialog implements RepositoryDialogInterface {
-  private static Class<?> PKG = PurRepositoryDialog.class; // for i18n purposes, needed by Translator2!!   $NON-NLS-1$
+  private RepositoryMeta repositoryMeta;
 
-  private Label        wlURL;
-  private Button       wbURL;
-  private Text         wURL;
+  private RepositoriesMeta repositoriesMeta;
 
-  private Label        wlManditoryComments;
-  private Button       wManditoryComments;
+  private Shell parent;
 
-  private Label        wlName;
-  private Text         wName;
+  private int style;
 
-  private Label        wlDescription;
-  private Text         wDescription;
+  private static final Class<?> CLZ = PurRepositoryDialog.class;
 
-  private Button wOK, wCancel;
-    private Listener lsOK, lsCancel;
+  private static Log log = LogFactory.getLog(PurRepositoryDialog.class);
 
-  private Display       display;
-  private Shell         shell;
-  private PropsUI         props;
-  
-  private PurRepositoryMeta   input;
-  // private RepositoriesMeta repositories;
-  
-  public PurRepositoryDialog(Shell parent, int style, RepositoryMeta repositoryMeta, RepositoriesMeta repositoriesMeta)
-  {
-    this.display = parent.getDisplay();
-    this.props=PropsUI.getInstance();
-    this.input = (PurRepositoryMeta) repositoryMeta;
-    // this.repositories = repositoriesMeta;
-    
-    shell = new Shell(parent, style | SWT.DIALOG_TRIM | SWT.RESIZE | SWT.MAX | SWT.MIN );
-    shell.setText(BaseMessages.getString(PKG, "PurRepositoryDialog.Dialog.Main.Title")); //$NON-NLS-1$
+  private RepositoryConfigController repositoryConfigController = new RepositoryConfigController();
+
+  private XulDomContainer container;
+
+  private ResourceBundle resourceBundle = new ResourceBundle() {
+
+    @Override
+    public Enumeration<String> getKeys() {
+      return null;
+    }
+
+    @Override
+    protected Object handleGetObject(String key) {
+      return BaseMessages.getString(CLZ, key);
+    }
+
+  };
+
+  public PurRepositoryDialog(Shell parent, int style, RepositoryMeta repositoryMeta,
+      RepositoriesMeta repositoriesMeta) {
+    this.parent = parent;
+    this.repositoriesMeta = repositoriesMeta;
+    this.repositoryMeta = repositoryMeta;
+    this.style = style;
   }
 
-  public PurRepositoryMeta open()
-  {
-    props.setLook(shell);
+  public RepositoryMeta open() {
+    try {
+      SwtXulLoader swtLoader = new SwtXulLoader();
+      swtLoader.setOuterContext(parent);
+      swtLoader.registerClassLoader(getClass().getClassLoader());
+      container = new SwtXulLoader().loadXul(
+          "org/pentaho/di/ui/repository/pur/xul/pur-repository-config-dialog.xul", resourceBundle); //$NON-NLS-1$
+      final XulRunner runner = new SwtXulRunner();
+      runner.addContainer(container);
+      parent.addDisposeListener(new DisposeListener(){
 
-    FormLayout formLayout = new FormLayout ();
-    formLayout.marginWidth  = Const.FORM_MARGIN;
-    formLayout.marginHeight = Const.FORM_MARGIN;
-
-    shell.setLayout(formLayout);
-    shell.setImage(GUIResource.getInstance().getImageSpoon());
-    shell.setText(BaseMessages.getString(PKG, "PurRepositoryDialog.Dialog.Main.Title2")); //$NON-NLS-1$
-    
-    int middle = props.getMiddlePct();
-    int margin = Const.MARGIN;
-
-    // Add the connection buttons :
-    wbURL = new Button(shell, SWT.PUSH);  wbURL.setText(BaseMessages.getString(PKG, "System.Button.Test")); //$NON-NLS-1$
-
-    FormData fdbBaseDir = new FormData();   
-    fdbBaseDir.right = new FormAttachment(100, 0);
-    fdbBaseDir.top   = new FormAttachment(0, margin);
-    wbURL.setLayoutData(fdbBaseDir);
-
-    wURL=new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-    props.setLook(wURL);
-    FormData fdBaseDir = new FormData();
-    fdBaseDir.left = new FormAttachment(middle, 0);
-    fdBaseDir.top  = new FormAttachment(wbURL, 0, SWT.CENTER);
-    fdBaseDir.right= new FormAttachment(wbURL, -margin);
-    wURL.setLayoutData(fdBaseDir);  
-
-        // Base directory line
-        wlURL=new Label(shell, SWT.RIGHT);
-        wlURL.setText(BaseMessages.getString(PKG, "PurRepositoryDialog.Label.URL")); //$NON-NLS-1$
-        props.setLook(wlURL);
-        FormData fdlBaseDir = new FormData();
-        fdlBaseDir.left = new FormAttachment(0, 0);
-        fdlBaseDir.right= new FormAttachment(middle, -margin);
-        fdlBaseDir.top  = new FormAttachment(wbURL, 0, SWT.CENTER);
-        wlURL.setLayoutData(fdlBaseDir);
-
-
-    // ManditoryComments line
-    wlManditoryComments=new Label(shell, SWT.RIGHT);
-    wlManditoryComments.setText(BaseMessages.getString(PKG, "PurRepositoryDialog.Label.ManditoryComments")); //$NON-NLS-1$
-    props.setLook(wlManditoryComments);
-    FormData fdlManditoryComments = new FormData();
-    fdlManditoryComments.left = new FormAttachment(0, 0);
-    fdlManditoryComments.top  = new FormAttachment(wURL, margin);
-    fdlManditoryComments.right= new FormAttachment(middle, -margin);
-    wlManditoryComments.setLayoutData(fdlManditoryComments);
-    wManditoryComments=new Button(shell, SWT.CHECK);
-    props.setLook(wManditoryComments);
-    FormData fdManditoryComments = new FormData();
-    fdManditoryComments.left = new FormAttachment(middle, 0);
-    fdManditoryComments.top  = new FormAttachment(wURL, margin);
-    fdManditoryComments.right= new FormAttachment(100, 0);
-    wManditoryComments.setLayoutData(fdManditoryComments);
-
-    
-    // Add the listeners
-    // New connection
-    wbURL.addSelectionListener(new SelectionAdapter() 
-      {
-        public void widgetSelected(SelectionEvent arg0) 
-        {
-          DirectoryDialog dialog = new DirectoryDialog(shell, SWT.NONE);
-          dialog.setText("Select root directory");
-          dialog.setMessage("Select the repository root directory");
-          String folder = dialog.open();
-          if (folder!=null) {
-            wURL.setText(folder);
-          }
+        public void widgetDisposed(DisposeEvent arg0) {
+          hide();
         }
-      }
-    );
-  
-    // Name line
-    wlName=new Label(shell, SWT.RIGHT);
-    wlName.setText(BaseMessages.getString(PKG, "PurRepositoryDialog.Label.Name")); //$NON-NLS-1$
-    props.setLook(wlName);
-    FormData fdlName = new FormData();
-    fdlName.left = new FormAttachment(0, 0);
-    fdlName.top  = new FormAttachment(wManditoryComments, margin*2);
-    fdlName.right= new FormAttachment(middle, -margin);
-    wlName.setLayoutData(fdlName);
-    wName=new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-    props.setLook(wName);
-    FormData fdName = new FormData();
-    fdName.left = new FormAttachment(middle, 0);
-    fdName.top  = new FormAttachment(wManditoryComments, margin*2);
-    fdName.right= new FormAttachment(100, 0);
-    wName.setLayoutData(fdName);
-
-    // Description line
-    wlDescription=new Label(shell, SWT.RIGHT);
-    wlDescription.setText(BaseMessages.getString(PKG, "PurRepositoryDialog.Label.Description")); //$NON-NLS-1$
-    props.setLook(wlDescription);
-    FormData fdlDescription = new FormData();
-    fdlDescription.left = new FormAttachment(0, 0);
-    fdlDescription.top  = new FormAttachment(wName, margin);
-    fdlDescription.right= new FormAttachment(middle, -margin);
-    wlDescription.setLayoutData(fdlDescription);
-    wDescription=new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-    props.setLook(wDescription);
-    FormData fdDescription = new FormData();
-    fdDescription.left = new FormAttachment(middle, 0);
-    fdDescription.top  = new FormAttachment(wName, margin);
-    fdDescription.right= new FormAttachment(100, 0);
-    wDescription.setLayoutData(fdDescription);
-
-
-    
-
-    wOK=new Button(shell, SWT.PUSH);
-    wOK.setText(BaseMessages.getString(PKG, "System.Button.OK")); //$NON-NLS-1$
-    lsOK       = new Listener() { public void handleEvent(Event e) { ok();     } };
-    wOK.addListener    (SWT.Selection, lsOK    );
-
-    wCancel=new Button(shell, SWT.PUSH);
-    wCancel.setText(BaseMessages.getString(PKG, "System.Button.Cancel")); //$NON-NLS-1$
-    lsCancel   = new Listener() { public void handleEvent(Event e) { cancel(); } };
-    wCancel.addListener(SWT.Selection, lsCancel);
-
-        BaseStepDialog.positionBottomButtons(shell, new Button[] { wOK, wCancel}, margin, wDescription);
-    // Detect X or ALT-F4 or something that kills this window...
-    shell.addShellListener( new ShellAdapter() { public void shellClosed(ShellEvent e) { cancel(); } } );
-
-    getData();
-
-    BaseStepDialog.setSize(shell);
-
-    shell.open();
-    while (!shell.isDisposed())
-    {
-        if (!display.readAndDispatch()) display.sleep();
-    }
-    return input;
-  }
-
-  public void dispose()
-  {
-    props.setScreen(new WindowProperty(shell));
-    shell.dispose();
-  }
-  
-  /**
-   * Copy information from the meta-data input to the dialog fields.
-   */ 
-  public void getData()
-  {
-    wName.setText(Const.NVL(input.getName(), ""));
-    wDescription.setText(Const.NVL(input.getDescription(), ""));
-    PurRepositoryLocation location = input.getRepositoryLocation();
-    if (location!=null) {
-      wURL.setText(Const.NVL(location.getUrl(), ""));
-    }else{
-      wURL.setText("http://localhost:8080/pentaho"); // default repo web service URL
-    }
-    wManditoryComments.setSelection(input.isVersionCommentMandatory());
-  }
-  
-  private void cancel()
-  {
-    input = null;
-    dispose();
-  }
-  
-  private void getInfo(PurRepositoryMeta info)
-  {
-    info.setName(wName.getText());
-    info.setDescription(wDescription.getText());
-    info.setRepositoryLocation(new PurRepositoryLocation(wURL.getText()));
-    info.setVersionCommentMandatory(wManditoryComments.getSelection());
-  }
-  
-  private void ok()
-  {
-    getInfo(input);
         
-        if (input.getName()!=null && input.getName().length()>0)
-        {
-            dispose();
+      });
+      repositoryConfigController.setRepositoryMeta(repositoryMeta);
+      repositoryConfigController.setCallback(new IRepositoryConfigDialogCallback() {
+
+        public void onSuccess(PurRepositoryMeta meta) {
+          repositoryMeta = meta;
+          hide();
         }
-        else
-        {
-            MessageBox box = new MessageBox(shell, SWT.ICON_ERROR | SWT.OK );
-            box.setMessage(BaseMessages.getString(PKG, "PurRepositoryDialog.Dialog.ErrorNoName.Message")); //$NON-NLS-1$
-            box.setText(BaseMessages.getString(PKG, "PurRepositoryDialog.Dialog.ErrorNoName.Title")); //$NON-NLS-1$
-            box.open();
-       }
+
+        public void onError(Throwable t) {
+          SpoonFactory.getInstance().messageBox(t.getLocalizedMessage(), resourceBundle.getString("RepositoryConfigDialog.InitializationFailed"), false, Const.ERROR); //$NON-NLS-1$
+          log.error(resourceBundle.getString("RepositoryConfigDialog.ErrorStartingXulApplication"), t);//$NON-NLS-1$
+        }
+
+        public void onCancel() {
+          repositoryMeta = null;
+          hide();
+        }
+      });
+      container.addEventHandler(repositoryConfigController);
+
+      try {
+        runner.initialize();
+        show();
+      } catch (XulException e) {
+        SpoonFactory.getInstance().messageBox(e.getLocalizedMessage(), resourceBundle.getString("RepositoryConfigDialog.InitializationFailed"), false, Const.ERROR); //$NON-NLS-1$
+        log.error(resourceBundle.getString("RepositoryConfigDialog.ErrorStartingXulApplication"), e);//$NON-NLS-1$
+      }
+    } catch (XulException e) {
+      log.error(resourceBundle.getString("RepositoryConfigDialog.ErrorStartingXulApplication"), e);//$NON-NLS-1$
+    }
+    return repositoryMeta;
   }
+
+  public Composite getDialogArea() {
+    XulDialog dialog = (XulDialog) container.getDocumentRoot().getElementById("repository-config-dialog"); //$NON-NLS-1$
+    return (Composite) dialog.getManagedObject();
+  }
+
+  public void show() {
+    XulDialog dialog = (XulDialog) container.getDocumentRoot().getElementById("repository-config-dialog"); //$NON-NLS-1$
+    dialog.show();
+  }
+  
+  public void hide() {
+    XulDialog dialog = (XulDialog) container.getDocumentRoot().getElementById("repository-config-dialog"); //$NON-NLS-1$
+    dialog.hide();
+  }
+
+  public Shell getShell() {
+    XulDialog dialog = (XulDialog) container.getDocumentRoot().getElementById("repository-config-dialog"); //$NON-NLS-1$
+    return (Shell) dialog.getRootObject();
+  }
+
 }
