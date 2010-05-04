35dddef07f1d6a4e3d93e369edc8683a95dd96d6
William Seyler <wseyler@pentaho.com>
2010-05-04 20:10:40 +0000

    PDIEE-137: Changed everywhere a JOptionPane was being used for reporting license key problem to a swt MessageBox.  This prevented problems with SWT and Swing not playing well together on the Mac.

--- src/org/pentaho/di/repository/pur/PluginLicenseVerifier.java
+++ src/org/pentaho/di/repository/pur/PluginLicenseVerifier.java
@@ -3,10 +3,11 @@ package org.pentaho.di.repository.pur;
 import java.util.Enumeration;
 import java.util.ResourceBundle;
 
-import javax.swing.JOptionPane;
-
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.ui.repository.EESpoonPlugin;
+import org.pentaho.di.ui.spoon.Spoon;
 
 import com.pentaho.commons.dsc.PentahoLicenseException;
 import com.pentaho.commons.dsc.PentahoLicenseVerifier;
@@ -34,15 +35,21 @@ public class PluginLicenseVerifier {
       PentahoLicenseVerifier.verify(new KParam());
     } catch (PentahoLicenseException ple) {
       ple.printStackTrace();
-      JOptionPane
-          .showMessageDialog(
-              null,
-              messages
-                  .getString(new ObfuscatedString(new long[] { 0x30E2E4A088DE4424L, 0x2D879C295D47AE8L,
-                      0xF6CA345B0F2FBE8FL, 0xD6519406AA783413L, 0x6BFF6F99D967AA4AL, 0x4FF2F7EC3E1F3D76L,
-                      0xD6E2BFFAACC4F634L, 0xD71C5E21ABE2A571L, 0xD0B595C3D287C676L }).toString() /* => "AbsSecurityProvider.ERROR_0005_INVALID_OR_MISSING_PRODUCT_LICENSE" */),
-              messages.getString(new ObfuscatedString(new long[] { 0x7C1A5C05F9D664C9L, 0x86938B18D4442883L,
-                  0x50E90B1A4F710720L }).toString() /* => "Dialog.Error" */), JOptionPane.ERROR_MESSAGE);
+      
+      MessageBox messagebox = new MessageBox(Spoon.getInstance().getShell(), SWT.ICON_ERROR | SWT.OK);
+      messagebox.setText(messages.getString(new ObfuscatedString(new long[] { 0x30E2E4A088DE4424L, 0x2D879C295D47AE8L, 0xF6CA345B0F2FBE8FL, 0xD6519406AA783413L, 0x6BFF6F99D967AA4AL, 0x4FF2F7EC3E1F3D76L, 0xD6E2BFFAACC4F634L, 0xD71C5E21ABE2A571L, 0xD0B595C3D287C676L }).toString() /* => "AbsSecurityProvider.ERROR_0005_INVALID_OR_MISSING_PRODUCT_LICENSE" */));
+      messagebox.setMessage(messages.getString(new ObfuscatedString(new long[] { 0x7C1A5C05F9D664C9L, 0x86938B18D4442883L,0x50E90B1A4F710720L }).toString() /* => "Dialog.Error" */));
+      messagebox.open();
+
+//      JOptionPane
+//          .showMessageDialog(
+//              null,
+//              messages
+//                  .getString(new ObfuscatedString(new long[] { 0x30E2E4A088DE4424L, 0x2D879C295D47AE8L,
+//                      0xF6CA345B0F2FBE8FL, 0xD6519406AA783413L, 0x6BFF6F99D967AA4AL, 0x4FF2F7EC3E1F3D76L,
+//                      0xD6E2BFFAACC4F634L, 0xD71C5E21ABE2A571L, 0xD0B595C3D287C676L }).toString() /* => "AbsSecurityProvider.ERROR_0005_INVALID_OR_MISSING_PRODUCT_LICENSE" */),
+//              messages.getString(new ObfuscatedString(new long[] { 0x7C1A5C05F9D664C9L, 0x86938B18D4442883L,
+//                  0x50E90B1A4F710720L }).toString() /* => "Dialog.Error" */), JOptionPane.ERROR_MESSAGE);
       System.exit(1);
     }
   }
