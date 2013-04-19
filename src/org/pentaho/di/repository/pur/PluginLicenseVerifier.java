486100d79aa868d201779d1c9f62151a81f1408e
Peter Minutillo <pminutillo@pentaho.com>
2013-04-19 20:06:21 +0000

    PDI-9095 - Error during startup without valid licenses - Removed System.exit() and instead added logging for license validation failure

--- src/org/pentaho/di/repository/pur/PluginLicenseVerifier.java
+++ src/org/pentaho/di/repository/pur/PluginLicenseVerifier.java
@@ -5,23 +5,21 @@
  */
 package org.pentaho.di.repository.pur;
 
-import java.util.Enumeration;
-import java.util.ResourceBundle;
-
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.MessageBox;
-import org.pentaho.di.i18n.BaseMessages;
-import org.pentaho.di.ui.repository.EESpoonPlugin;
-import org.pentaho.di.ui.spoon.Spoon;
-
 import com.pentaho.commons.dsc.PentahoLicenseException;
 import com.pentaho.commons.dsc.PentahoLicenseVerifier;
 import com.pentaho.commons.dsc.params.KParam;
-import com.pentaho.commons.dsc.tlsup.util.ObfuscatedString;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.ui.repository.EESpoonPlugin;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
 
 public class PluginLicenseVerifier implements java.io.Serializable {
 
   private static final long serialVersionUID = 7324261459822440599L; /* EESOURCE: UPDATE SERIALVERUID */
+  private static final Log logger = LogFactory.getLog(PluginLicenseVerifier.class);
 
   private static ResourceBundle messages = new ResourceBundle() {
 
@@ -41,14 +39,14 @@ public class PluginLicenseVerifier implements java.io.Serializable {
     try {
       PentahoLicenseVerifier.verify(new KParam(PurRepositoryMeta.BUNDLE_REF_NAME));
     } catch (PentahoLicenseException ple) {
-      ple.printStackTrace();
-      
-      MessageBox messagebox = new MessageBox(Spoon.getInstance().getShell(), SWT.ICON_ERROR | SWT.OK);
-      messagebox.setText(messages.getString(new ObfuscatedString(new long[] { 0x7C1A5C05F9D664C9L, 0x86938B18D4442883L,0x50E90B1A4F710720L }).toString() /* => "Dialog.Error" */));
-      messagebox.setMessage(messages.getString(new ObfuscatedString(new long[] { 0x30E2E4A088DE4424L, 0x2D879C295D47AE8L, 0xF6CA345B0F2FBE8FL, 0xD6519406AA783413L, 0x6BFF6F99D967AA4AL, 0x4FF2F7EC3E1F3D76L, 0xD6E2BFFAACC4F634L, 0xD71C5E21ABE2A571L, 0xD0B595C3D287C676L }).toString() /* => "AbsSecurityProvider.ERROR_0005_INVALID_OR_MISSING_PRODUCT_LICENSE" */));
-      messagebox.open();
-      System.exit(1);
+      // simply log the license verification failure
+      getLogger().error(
+          BaseMessages.getString(PluginLicenseVerifier.class,
+              "PluginLicenseVerifier.LicenseValidationFailed.Log")); //$NON-NLS-1$
     }
   }
 
+  public static Log getLogger() {
+    return logger;
+  }
 }
