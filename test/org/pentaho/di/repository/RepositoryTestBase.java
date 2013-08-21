5fceeee00a42caec53af7b992daf169d7a204d61
Matt Casters <mcasters@pentaho.com>
2013-08-21 11:42:05 +0000

    Unit test fix for directory rename and move (can't be done at the same time!)

--- test/org/pentaho/di/repository/RepositoryTestBase.java
+++ test/org/pentaho/di/repository/RepositoryTestBase.java
@@ -472,7 +472,9 @@ public abstract class RepositoryTestBase {
 
     RepositoryDirectoryInterface moveTestDestDir = repository.createRepositoryDirectory(startDir, "moveTestDest");
     RepositoryDirectoryInterface moveTestSrcDir = repository.createRepositoryDirectory(startDir, "moveTestSrc");
-    repository.renameRepositoryDirectory(moveTestSrcDir.getObjectId(), moveTestDestDir, "moveTestSrcNewName");
+    // First rename, then move the folder
+    repository.renameRepositoryDirectory(moveTestSrcDir.getObjectId(), null, "moveTestSrcNewName");
+    repository.renameRepositoryDirectory(moveTestSrcDir.getObjectId(), moveTestDestDir, null);
     startDir = loadStartDirectory();
     assertNull(startDir.findDirectory("moveTestSrc"));
     assertNotNull(startDir.findDirectory("moveTestDest/moveTestSrcNewName"));
