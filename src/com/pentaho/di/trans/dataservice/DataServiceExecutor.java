ddadf186e4faf8ff82d181eee9a576cd906c9595
Nick Hudak <nhudak@pentaho.com>
2014-08-25 14:23:36 -0400

    [BACKLOG-559]Port SqlTransExecutor to EE
    Created DataServiceExecutor
    Functionality is identical to SqlTransExecutor
    Removed dead code blocks pertaining to FieldVariableMappings and Caching
    
    Unit Test included

---
+++ src/com/pentaho/di/trans/dataservice/DataServiceExecutor.java
@@ -0,0 +1,353 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.pentaho.di.trans.dataservice;
+
+import org.pentaho.di.core.Condition;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.core.exception.KettleValueException;
+import org.pentaho.di.core.logging.LogChannelInterface;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.sql.SQL;
+import org.pentaho.di.repository.Repository;
+import org.pentaho.di.repository.StringObjectId;
+import org.pentaho.di.trans.RowProducer;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransAdapter;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.sql.SqlTransMeta;
+import org.pentaho.di.trans.step.RowAdapter;
+import org.pentaho.di.trans.step.RowListener;
+import org.pentaho.di.trans.step.StepInterface;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class DataServiceExecutor {
+  private Trans serviceTrans;
+  private Trans genTrans;
+
+  private List<DataServiceMeta> services;
+  private String serviceName;
+  private DataServiceMeta service;
+  private SQL sql;
+  private Repository repository;
+  private int rowLimit;
+  private Map<String, String> parameters;
+  private boolean dual;
+  private SqlTransMeta sqlTransGenerator;
+
+  // Initialize empty without prepareExecution
+  protected DataServiceExecutor() {
+    parameters = Collections.emptyMap();
+  }
+
+  /**
+   * @param sqlQuery User SQL query
+   * @param services Available services
+   * @param parameters Connection trans parameters
+   * @param repository Repository to search for transformation
+   * @throws KettleException
+   */
+  public DataServiceExecutor( String sqlQuery, List<DataServiceMeta> services, Map<String, String> parameters,
+                              Repository repository, int rowLimit ) throws KettleException {
+    this.services = services;
+    this.parameters = parameters;
+    this.repository = repository;
+    this.rowLimit = rowLimit;
+    this.sql = new SQL( sqlQuery );
+    this.serviceName = sql.getServiceName();
+
+    prepareExecution();
+  }
+
+  public void prepareExecution() throws KettleException {
+    // First see if this is a special "dual" table we're reading from...
+    //
+    RowMetaInterface serviceFields;
+    if ( Const.isEmpty( serviceName ) || "dual".equalsIgnoreCase( serviceName ) ) {
+      service = new DataServiceMeta();
+      service.setName( "dual" );
+      sql.setServiceName( "dual" );
+      setDual( true );
+      serviceFields = new RowMeta(); // nothing to report from dual
+    } else {
+      // Locate Data Service
+      service = findService( serviceName );
+      if ( service == null ) {
+        throw new KettleException( "Unable to find service with name '" + serviceName + "' and SQL: " + sql.getSqlString() );
+      }
+
+      // Load Service Trans
+      TransMeta serviceTransMeta = loadTransMeta( repository );
+      serviceTransMeta.setName( calculateTransname( sql, true ) );
+
+      serviceTransMeta.activateParameters();
+      serviceFields = serviceTransMeta.getStepFields( service.getStepname() );
+      serviceTrans = new Trans( serviceTransMeta );
+    }
+    // Continue parsing of the SQL, map to fields, extract conditions, parameters, ...
+    //
+    sql.parse( serviceFields );
+
+    // Generate a transformation
+    //
+    sqlTransGenerator = new SqlTransMeta( sql, rowLimit );
+    TransMeta genTransMeta = sqlTransGenerator.generateTransMeta();
+    genTrans = new Trans( genTransMeta );
+  }
+
+  private DataServiceMeta findService( String name ) {
+    for ( DataServiceMeta s : services ) {
+      if ( s.getName().equalsIgnoreCase( name ) ) {
+        return s;
+      }
+    }
+    return null;
+  }
+
+  private void extractConditionParameters( Condition condition, Map<String, String> map ) {
+
+    if ( condition.isAtomic() ) {
+      if ( condition.getFunction() == Condition.FUNC_TRUE ) {
+        map.put( condition.getLeftValuename(), condition.getRightExactString() );
+      }
+    } else {
+      for ( Condition sub : condition.getChildren() ) {
+        extractConditionParameters( sub, map );
+      }
+    }
+  }
+
+  public void executeQuery( RowListener resultRowListener ) throws KettleException {
+
+    genTrans.prepareExecution( null );
+
+    if ( !isDual() ) {
+      // Parameters: see which ones are defined in the SQL
+      //
+      Map<String, String> conditionParameters = new HashMap<String, String>();
+      if ( sql.getWhereCondition() != null ) {
+        extractConditionParameters( sql.getWhereCondition().getCondition(), conditionParameters );
+      }
+      parameters.putAll( conditionParameters ); // overwrite the defaults for this query
+
+      TransMeta serviceTransMeta = getServiceTransMeta();
+      for ( Entry<String, String> parameter : parameters.entrySet() ) {
+        serviceTransMeta.setParameterValue( parameter.getKey(), parameter.getValue() );
+      }
+
+      serviceTrans.prepareExecution( null );
+
+      // This is where we will inject the rows from the service transformation step
+      //
+      final RowProducer rowProducer = genTrans.addRowProducer( sqlTransGenerator.getInjectorStepName(), 0 );
+
+      // Now connect the 2 transformations with listeners and injector
+      //
+      StepInterface serviceStep = serviceTrans.findRunThread( service.getStepname() );
+      serviceStep.addRowListener( new RowAdapter() {
+        @Override
+        public void rowWrittenEvent( RowMetaInterface rowMeta, Object[] row ) throws KettleStepException {
+          // Simply pass along the row to the other transformation (to the Injector step)
+          //
+          LogChannelInterface log = serviceTrans.getLogChannel();
+          try {
+            if ( log.isRowLevel() ) {
+              log.logRowlevel( "Passing along row: " + rowMeta.getString( row ) );
+            }
+          } catch ( KettleValueException e ) {
+            // Ignore errors
+          }
+
+          rowProducer.putRow( rowMeta, row );
+        }
+      } );
+
+      // Let the other transformation know when there are no more rows
+      //
+      serviceTrans.addTransListener( new TransAdapter() {
+        @Override
+        public void transFinished( Trans trans ) throws KettleException {
+          rowProducer.finished();
+        }
+      } );
+    }
+
+    // Give back the eventual result rows...
+    //
+    StepInterface resultStep = genTrans.findRunThread( getResultStepName() );
+    resultStep.addRowListener( resultRowListener );
+
+    // Start both transformations
+    //
+    genTrans.startThreads();
+    if ( !isDual() ) {
+      serviceTrans.startThreads();
+    }
+  }
+
+  private TransMeta loadTransMeta( Repository repository ) throws KettleException {
+    TransMeta transMeta;
+
+    if ( !Const.isEmpty( service.getTransFilename() ) ) {
+      try {
+        // OK, load the meta-data from file...
+        //
+        // Don't set internal variables: they belong to the parent thread!
+        //
+        transMeta = new TransMeta( service.getTransFilename(), false );
+        transMeta.getLogChannel().logDetailed(
+          "Service transformation was loaded from XML file [" + service.getTransFilename() + "]" );
+      } catch ( Exception e ) {
+        throw new KettleException( "Unable to load service transformation for service '" + serviceName + "'", e );
+      }
+    } else {
+      try {
+        StringObjectId objectId = new StringObjectId( service.getTransObjectId() );
+        transMeta = repository.loadTransformation( objectId, null );
+        transMeta.getLogChannel().logDetailed(
+          "Service transformation was loaded from repository for service [" + service.getName() + "]" );
+      } catch ( Exception e ) {
+        throw new KettleException( "Unable to load service transformation for service '"
+          + serviceName + "' from the repository", e );
+      }
+    }
+    return transMeta;
+  }
+
+  public void waitUntilFinished() {
+    if ( !isDual() ) {
+      serviceTrans.waitUntilFinished();
+    }
+    genTrans.waitUntilFinished();
+  }
+
+  /**
+   * @return the serviceTransMeta
+   */
+  public TransMeta getServiceTransMeta() {
+    return isDual() ? null : serviceTrans.getTransMeta();
+  }
+
+  /**
+   * @return the genTransMeta
+   */
+  public TransMeta getGenTransMeta() {
+    return genTrans.getTransMeta();
+  }
+
+  /**
+   * @return the serviceTrans
+   */
+  public Trans getServiceTrans() {
+    return serviceTrans;
+  }
+
+  /**
+   * @return the genTrans
+   */
+  public Trans getGenTrans() {
+    return genTrans;
+  }
+
+  /**
+   * @return the serviceName
+   */
+  public String getServiceName() {
+    return serviceName;
+  }
+
+  /**
+   * Calculate the name of the generated transformation based on the SQL
+   *
+   * @return the generated name;
+   */
+  public static String calculateTransname( SQL sql, boolean isService ) {
+    StringBuilder sbsql = new StringBuilder( sql.getServiceName() );
+    sbsql.append( " - " );
+    if ( isService ) {
+      sbsql.append( "Service" );
+    } else {
+      sbsql.append( "SQL" );
+    }
+    sbsql.append( " - " );
+    sbsql.append( sql.getSqlString() );
+
+    // Get rid of newlines...
+    //
+    for ( int i = sbsql.length() - 1; i >= 0; i-- ) {
+      if ( sbsql.charAt( i ) == '\n' || sbsql.charAt( i ) == '\r' ) {
+        sbsql.setCharAt( i, ' ' );
+      }
+    }
+    return sbsql.toString();
+  }
+
+  /**
+   * @return the sql
+   */
+  public SQL getSql() {
+    return sql;
+  }
+
+  public void setSql( SQL sql ) {
+    this.sql = sql;
+  }
+
+  /**
+   * @return the resultStepName
+   */
+  public String getResultStepName() {
+    return sqlTransGenerator.getResultStepName();
+  }
+
+  public void setGenTrans( Trans genTrans ) {
+    this.genTrans = genTrans;
+  }
+
+  public void setServiceTrans( Trans serviceTrans ) {
+    this.serviceTrans = serviceTrans;
+  }
+
+  public void setService( DataServiceMeta service ) {
+    this.service = service;
+  }
+
+  public void setSqlTransMeta( SqlTransMeta sqlTransMeta ) {
+    this.sqlTransGenerator = sqlTransMeta;
+  }
+
+  public boolean isDual() {
+    return dual;
+  }
+
+  public void setDual( boolean dual ) {
+    this.dual = dual;
+  }
+}
---
+++ test/com/pentaho/di/trans/dataservice/DataServiceExecutorTest.java
@@ -0,0 +1,227 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2013 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.pentaho.di.trans.dataservice;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.sql.SQL;
+import org.pentaho.di.repository.ObjectId;
+import org.pentaho.di.repository.Repository;
+import org.pentaho.di.repository.StringObjectId;
+import org.pentaho.di.trans.RowProducer;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransListener;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.sql.SqlTransMeta;
+import org.pentaho.di.trans.step.RowListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DataServiceExecutorTest {
+
+  public static final String SERVICE_NAME = "serviceName";
+  public static final String SERVICE_STEP_NAME = "Service Step";
+  public static final String INJECTOR_STEP_NAME = "Injector Step";
+  public static final String RESULT_STEP_NAME = "Result Step";
+
+  @Test
+  public void testCreateExecutorByObjectId() throws Exception {
+    String query = "SELECT field1, field2 FROM " + SERVICE_NAME;
+
+    DataServiceMeta service = mockDataServiceMeta();
+    ObjectId transId = new StringObjectId( UUID.randomUUID().toString() );
+    when( service.getTransObjectId() ).thenReturn( transId.getId() );
+
+    TransMeta trans = mockTransMeta();
+
+    Repository repository = mock( Repository.class );
+    when( repository.loadTransformation( eq( transId ), anyString() ) ).thenReturn( trans );
+
+    Map<String, String> parameters = Collections.emptyMap();
+
+    List<DataServiceMeta> services = createServicesList( service );
+    DataServiceExecutor executor = new DataServiceExecutor( query, services, parameters, repository, 0 );
+
+    // Verify execution prep
+    assertEquals( trans, executor.getServiceTransMeta() );
+    assertNotNull( executor.getGenTransMeta() );
+    assertNotNull( "Service Trans not created", executor.getServiceTrans() );
+    assertNotNull( "SQL Trans not created", executor.getGenTrans() );
+
+    // Verify Properties
+    assertEquals( SERVICE_NAME, executor.getServiceName() );
+    assertNotNull( executor.getSql() );
+    assertNotNull( executor.getResultStepName() );
+  }
+
+  @Test
+  public void testDual() throws Exception {
+    ArrayList<String> queries = Lists.newArrayList( "Select 1,2,3 from DUAL", "Select 1,2,3" );
+    List<DataServiceMeta> services = Collections.emptyList();
+    Map<String, String> parameters = Collections.emptyMap();
+    Repository repository = mock( Repository.class );
+
+    for ( String query : queries ) {
+      DataServiceExecutor executor = new DataServiceExecutor( query, services, parameters, repository, 0 );
+
+      // Verify execution prep
+      assertNull( executor.getServiceTrans() );
+      assertNull( executor.getServiceTransMeta() );
+      assertNotNull( executor.getGenTransMeta() );
+      assertNotNull( executor.getGenTrans() );
+      assertTrue( executor.isDual() );
+
+      Trans genTrans = mock( Trans.class, RETURNS_DEEP_STUBS );
+      executor.setGenTrans( genTrans );
+      SqlTransMeta sqlTransMeta = mockSqlMetaTrans();
+      executor.setSqlTransMeta( sqlTransMeta );
+
+      RowListener clientRowListener = mock( RowListener.class );
+
+      // Start Execution
+      executor.executeQuery( clientRowListener );
+
+      InOrder startup = inOrder( genTrans, genTrans.findRunThread( RESULT_STEP_NAME ) );
+
+      startup.verify( genTrans ).prepareExecution( null );
+      startup.verify( genTrans.findRunThread( RESULT_STEP_NAME ) ).addRowListener( clientRowListener );
+      startup.verify( genTrans ).startThreads();
+    }
+  }
+
+  @Test
+  public void testExecuteQuery() throws Exception {
+    DataServiceExecutor executor = new DataServiceExecutor();
+
+    DataServiceMeta service = mockDataServiceMeta();
+    executor.setService( service );
+    Trans serviceTrans = mock( Trans.class, RETURNS_DEEP_STUBS );
+    executor.setServiceTrans( serviceTrans );
+    Trans genTrans = mock( Trans.class, RETURNS_DEEP_STUBS );
+    executor.setGenTrans( genTrans );
+    SQL sql = mock( SQL.class );
+    executor.setSql( sql );
+    SqlTransMeta sqlTransMeta = mockSqlMetaTrans();
+    executor.setSqlTransMeta( sqlTransMeta );
+
+    when( sql.getWhereClause() ).thenReturn( null );
+    when( serviceTrans.getTransMeta().listParameters() ).thenReturn( new String[0] );
+
+    RowListener clientRowListener = mock( RowListener.class );
+
+    RowProducer sqlTransRowProducer = genTrans.addRowProducer( INJECTOR_STEP_NAME, 0 );
+
+    // Start Execution
+    executor.executeQuery( clientRowListener );
+
+    InOrder genTransStartup = inOrder( genTrans, genTrans.findRunThread( RESULT_STEP_NAME ) );
+    InOrder serviceTransStartup = inOrder( serviceTrans, serviceTrans.findRunThread( SERVICE_STEP_NAME ) );
+    ArgumentCaptor<RowListener> listenerArgumentCaptor = ArgumentCaptor.forClass( RowListener.class );
+
+    genTransStartup.verify( genTrans ).prepareExecution( null );
+    genTransStartup.verify( genTrans ).addRowProducer( INJECTOR_STEP_NAME, 0 );
+    genTransStartup.verify( genTrans.findRunThread( RESULT_STEP_NAME ) ).addRowListener( clientRowListener );
+    genTransStartup.verify( genTrans ).startThreads();
+
+    serviceTransStartup.verify( serviceTrans ).prepareExecution( null );
+    serviceTransStartup.verify( serviceTrans.findRunThread( SERVICE_STEP_NAME ) ).addRowListener( listenerArgumentCaptor.capture() );
+    serviceTransStartup.verify( serviceTrans ).startThreads();
+
+    // Verify linkage
+    RowListener serviceRowListener = listenerArgumentCaptor.getValue();
+    assertNotNull( serviceRowListener );
+
+    // Push row from service to sql Trans
+    for ( int i = 0; i < 50; i++ ) {
+      RowMeta rowMeta =  mock( RowMeta.class );
+      Object[] data = new Object[0];
+
+      serviceRowListener.rowWrittenEvent( rowMeta, data );
+      verify( sqlTransRowProducer ).putRow( same( rowMeta ), same( data ) );
+    }
+
+    // Verify Service Trans finished
+    ArgumentCaptor<TransListener> serviceTransListener = ArgumentCaptor.forClass( TransListener.class );
+    verify( serviceTrans ).addTransListener( serviceTransListener.capture() );
+    serviceTransListener.getValue().transFinished( serviceTrans );
+    verify( sqlTransRowProducer ).finished();
+  }
+
+  private SqlTransMeta mockSqlMetaTrans() {
+    SqlTransMeta sqlTransMeta = mock( SqlTransMeta.class );
+    when( sqlTransMeta.getInjectorStepName() ).thenReturn( INJECTOR_STEP_NAME );
+    when( sqlTransMeta.getResultStepName() ).thenReturn( RESULT_STEP_NAME );
+    return sqlTransMeta;
+  }
+
+  private ArrayList<DataServiceMeta> createServicesList( DataServiceMeta service ) {
+    ArrayList<DataServiceMeta> list = Lists.newArrayListWithCapacity( 20 );
+    for( int i = 0; i < 19; i++ ) {
+      DataServiceMeta falseMeta = mock( DataServiceMeta.class );
+      when( falseMeta.getName() ).thenReturn( UUID.randomUUID().toString() );
+      list.add( falseMeta );
+    }
+    list.add( service );
+    return list;
+  }
+
+  private TransMeta mockTransMeta() throws KettleStepException {
+    TransMeta trans = mock( TransMeta.class, RETURNS_DEEP_STUBS );
+    RowMetaInterface serviceStep = mock( RowMetaInterface.class, RETURNS_DEEP_STUBS );
+    when( trans.getStepFields( SERVICE_STEP_NAME ) ).thenReturn( serviceStep );
+    when( trans.listVariables() ).thenReturn( new String[0] );
+    when( trans.listParameters() ).thenReturn( new String[0] );
+    return trans;
+  }
+
+  private DataServiceMeta mockDataServiceMeta() {
+    DataServiceMeta service = mock( DataServiceMeta.class );
+    when( service.getName() ).thenReturn( SERVICE_NAME );
+    when( service.getStepname() ).thenReturn( SERVICE_STEP_NAME );
+    return service;
+  }
+
+
+}
