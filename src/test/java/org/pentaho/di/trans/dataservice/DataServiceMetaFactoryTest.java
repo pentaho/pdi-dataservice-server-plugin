32c7eabc2d3ababdfd2a1f5e912458119220b730
Kris Rivera <krivera@pentaho.com>
2017-01-12 16:55:39 -0500

    [BACKLOG-13441] -- added DataServiceMetaFactory and associated bean services

---
+++ src/test/java/org/pentaho/di/trans/dataservice/DataServiceMetaFactoryTest.java
@@ -0,0 +1,144 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.trans.dataservice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.pentaho.di.repository.ObjectId;
+import org.pentaho.di.repository.Repository;
+import org.pentaho.di.repository.RepositoryDirectoryInterface;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.dataservice.optimization.cache.ServiceCache;
+import org.pentaho.di.trans.dataservice.optimization.cache.ServiceCacheFactory;
+import org.pentaho.di.trans.dataservice.resolvers.TransientResolver;
+import org.pentaho.di.trans.dataservice.serialization.SynchronizationListener;
+import org.pentaho.di.trans.dataservice.ui.DataServiceDelegate;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.ui.spoon.Spoon;
+import org.pentaho.di.ui.spoon.trans.TransGraph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+
+@RunWith( MockitoJUnitRunner.class )
+public class DataServiceMetaFactoryTest {
+  DataServiceMetaFactory factory;
+
+  @Mock
+  Context dataServiceContext;
+  @Mock
+  DataServiceDelegate dataServiceDelegate;
+  @Mock
+  Spoon mockSpoon;
+  @Mock
+  TransMeta transMeta;
+  @Mock
+  Repository repository;
+  @Mock
+  DataServiceMeta mockDs;
+  @Mock
+  TransGraph transGraph;
+  @Mock
+  StepMeta stepMeta;
+  @Mock
+  SynchronizationListener synchronizationListener;
+  @Mock
+  ServiceCacheFactory cacheFactory;
+
+  String transName = "Test Trans Name";
+  String stepName = "Test Step Name";
+
+  @Before
+  public void setup() {
+    factory = new DataServiceMetaFactory();
+    factory.setCacheFactory( cacheFactory );
+
+    when( dataServiceContext.getDataServiceDelegate() ).thenReturn( dataServiceDelegate );
+    when( dataServiceDelegate.createSyncService() ).thenReturn( synchronizationListener );
+    when( dataServiceDelegate.getSpoon() ).thenReturn( mockSpoon );
+
+    when( transMeta.getName() ).thenReturn( transName );
+    when( cacheFactory.createPushDown() ).thenReturn( new ServiceCache( cacheFactory ) );
+
+    when( stepMeta.getName() ).thenReturn( stepName );
+    when( stepMeta.getParentTransMeta() ).thenReturn( transMeta );
+  }
+
+  @Test
+  public void testCreateDataService() throws Exception {
+    when( mockSpoon.getRepository() ).thenReturn( repository );
+
+    DataServiceMeta ds = factory.createDataService( stepMeta );
+    assertNotNull( ds );
+    assertTrue( TransientResolver.isTransient( ds.getName() ) );
+    assertEquals( transMeta, ds.getServiceTrans() );
+    assertEquals( stepName, ds.getStepname() );
+    assertEquals( 1, ds.getPushDownOptimizationMeta().size() );
+    assertTrue( ds.getPushDownOptimizationMeta().get( 0 ).getType() instanceof ServiceCache );
+    assertEquals( stepName, ds.getPushDownOptimizationMeta().get( 0 ).getStepName() );
+  }
+
+  @Test
+  public void testCreateDataServiceName() throws Exception {
+    when( transMeta.getObjectId() ).thenReturn( null );
+    when( transMeta.getFilename() ).thenReturn( "C:\\windows\\path\\file.ktr" );
+    DataServiceMeta ds1 = factory.createDataService( stepMeta );
+    assertTrue( TransientResolver.isTransient( ds1.getName() ) );
+
+    when( transMeta.getFilename() ).thenReturn( "/unix/path/file.ktr" );
+    DataServiceMeta ds2 = factory.createDataService( stepMeta );
+    assertTrue( TransientResolver.isTransient( ds2.getName() ) );
+
+    RepositoryDirectoryInterface repositoryDirectoryInterface = mock( RepositoryDirectoryInterface.class );
+    when( repositoryDirectoryInterface.getPath() ).thenReturn( "/repository/path" );
+    when( transMeta.getObjectId() ).thenReturn( mock( ObjectId.class ) );
+    when( transMeta.getFilename() ).thenReturn( "/unix/path/file.ktr" );
+    when( transMeta.getRepositoryDirectory() ).thenReturn( repositoryDirectoryInterface );
+    DataServiceMeta ds3 = factory.createDataService( stepMeta );
+    assertTrue( TransientResolver.isTransient( ds3.getName() ) );
+
+    repositoryDirectoryInterface = mock( RepositoryDirectoryInterface.class );
+    when( repositoryDirectoryInterface.getPath() ).thenReturn( "/repository/path" );
+    when( transMeta.getFilename() ).thenReturn( "" );
+    when( transMeta.getRepositoryDirectory() ).thenReturn( repositoryDirectoryInterface );
+    DataServiceMeta ds6 = factory.createDataService( stepMeta );
+    assertTrue( TransientResolver.isTransient( ds6.getName() ) );
+
+    when( repositoryDirectoryInterface.getPath() ).thenReturn( "/repository/path/" );
+    when( transMeta.getFilename() ).thenReturn( "" );
+    when( transMeta.getRepositoryDirectory() ).thenReturn( repositoryDirectoryInterface );
+    DataServiceMeta ds4 = factory.createDataService( stepMeta );
+    assertTrue( TransientResolver.isTransient( ds4.getName() ) );
+
+    when( transMeta.getRepositoryDirectory() ).thenReturn( null );
+    DataServiceMeta ds5 = factory.createDataService( stepMeta );
+    assertTrue( TransientResolver.isTransient( ds5.getName() ) );
+  }
+
+}
